a92efc7bb6b65ae01205aad3a24ded17531ed8c6
Selim Utku Sönmez <selimutkusonmez70@gmail.com>
2026-02-20 23:24:50 +0300

    app files and folders created

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STYLE_PATH = os.path.join(BASE_DIR,"modules","assets","style")
+
+JPG_PATH = os.path.join(BASE_DIR,"modules","assets","image")
