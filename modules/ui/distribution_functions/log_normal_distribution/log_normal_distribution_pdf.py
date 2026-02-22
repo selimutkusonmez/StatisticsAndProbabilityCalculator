f0cc759d01906c80b6fce347ccf28ad8b502c5be
Selim Utku Sönmez <selimutkusonmez70@gmail.com>
2026-02-23 01:28:15 +0300

    log normal distribution done

--- modules/ui/distribution_functions/log_normal_distribution/log_normal_distribution_pdf.py
+++ modules/ui/distribution_functions/log_normal_distribution/log_normal_distribution_pdf.py
@@ -1,10 +1,8 @@
 from PyQt6.QtCore import Qt,QRegularExpression,QSize
 from PyQt6.QtWidgets import QWidget,QLineEdit,QPushButton,QLabel,QGridLayout,QGroupBox,QComboBox,QListWidget,QTextEdit,QVBoxLayout
 from PyQt6.QtGui import QIcon,QIntValidator,QDoubleValidator,QRegularExpressionValidator
-
-from config import STYLE_PATH
-
-from modules.logic.style_reader.style_reader import read_style
+import math
+import statistics
 
 class OperationWidget(QWidget):
     def __init__(self,operation_name):
@@ -19,37 +17,38 @@ class OperationWidget(QWidget):
 
         self.current_result = "<i>Waiting...</i>"
 
+        self.regex = QRegularExpressionValidator(QRegularExpression("[-0-9. ]+"))
+
         #Left GroupBox
         self.left_group_box = QGroupBox()
         self.left_group_box_layout = QGridLayout()
         self.left_group_box.setLayout(self.left_group_box_layout)
         self.layout.addWidget(self.left_group_box,0,0)
 
-        self.left_group_box.setFixedWidth(125)
+        self.left_group_box.setFixedWidth(250)
 
-        self.variable_1 = QLabel("1")
-        self.left_group_box_layout.addWidget(self.variable_1,0,0)
+        self.variable_1_label = QLabel("Data")
+        self.variable_1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.left_group_box_layout.addWidget(self.variable_1_label,0,0,1,2)
 
-        self.variable_1_input = QLineEdit()
-        self.left_group_box_layout.addWidget(self.variable_1_input,0,1)
+        self.variable_1_input = QTextEdit()
+        self.variable_1_input.setPlaceholderText("Seperated by comma")
+        self.left_group_box_layout.addWidget(self.variable_1_input,1,0,1,2)
 
-        self.variable_2 = QLabel("2")
-        self.left_group_box_layout.addWidget(self.variable_2,1,0)
+        self.variable_2_label = QLabel("x")
+        self.left_group_box_layout.addWidget(self.variable_2_label,2,0)
 
         self.variable_2_input = QLineEdit()
-        self.left_group_box_layout.addWidget(self.variable_2_input,1,1)
+        self.variable_2_input.setValidator(self.regex)
+        self.variable_2_input.setPlaceholderText("Target Value")
+        self.left_group_box_layout.addWidget(self.variable_2_input,2,1)
 
-        self.variable_3 = QLabel("3")
-        self.left_group_box_layout.addWidget(self.variable_3,2,0)
+        self.variable_3_label = QLabel("Dataset Type")
+        self.left_group_box_layout.addWidget(self.variable_3_label,3,0)
 
-        self.variable_3_input = QLineEdit()
-        self.left_group_box_layout.addWidget(self.variable_3_input,2,1)
-
-        self.variable_4 = QLabel("4")
-        self.left_group_box_layout.addWidget(self.variable_4,3,0)
-
-        self.variable_4_input = QLineEdit()
-        self.left_group_box_layout.addWidget(self.variable_4_input,3,1)
+        self.variable_3_input = QComboBox()
+        self.variable_3_input.addItems(["Population","Sample"])
+        self.left_group_box_layout.addWidget(self.variable_3_input,3,1)
 
         self.calculate_button = QPushButton("Calculate")
         self.calculate_button.clicked.connect(self.calculate_button_function)
@@ -81,36 +80,44 @@ class OperationWidget(QWidget):
         self.right_group_box.setLayout(self.right_group_box_layout)
         self.layout.addWidget(self.right_group_box,0,2)
 
-        self.right_group_box.setFixedWidth(225)
+        self.right_group_box.setFixedWidth(300)
 
-        self.variable_1_info_label = QLabel("1")
+        self.variable_1_info_label = QLabel()
         self.right_group_box_layout.addWidget(self.variable_1_info_label,0,0)
 
-        self.variable_1_info = QTextEdit()
+        self.variable_1_info = QTextEdit("<b>Log-Normal Distribution:</b><br>"
+                                        "A continuous probability distribution of a random variable whose <b>logarithm is normally distributed</b>. "
+                                        "Unlike the normal distribution, it is skewed to the right and only exists for <b>positive values (x > 0)</b>.<br><br>"
+                                        "<i>Commonly used for values that cannot be negative, such as stock prices, income, or rainfall amounts.</i>")
         self.variable_1_info.setReadOnly(True)
         self.right_group_box_layout.addWidget(self.variable_1_info,0,1)
 
 
-        self.variable_2_info_label = QLabel("2")
+        self.variable_2_info_label = QLabel("<i>&mu;</i>")
         self.right_group_box_layout.addWidget(self.variable_2_info_label,1,0)
-
-        self.variable_2_info = QTextEdit()
+        
+        self.variable_2_info = QTextEdit("<b>&mu; (Mu / Mean):</b><br>"
+                                        "The central or typical value of the distribution. It dictates where the peak of the bell curve is located. "
+                                        "It can be any real number")
         self.variable_2_info.setReadOnly(True)
         self.right_group_box_layout.addWidget(self.variable_2_info,1,1)
 
 
-        self.variable_3_info_label = QLabel("3")
+        self.variable_3_info_label = QLabel("<i>&sigma;</i>")
         self.right_group_box_layout.addWidget(self.variable_3_info_label,2,0)
 
-        self.variable_3_info = QTextEdit()
+        self.variable_3_info = QTextEdit("<b>&sigma; (Sigma / Standard Deviation):</b><br>"
+                                        "A measure of the amount of variation or dispersion. It determines how 'fat' or 'skinny' the bell curve is. "
+                                        "This must be strictly greater than 0 (<b>&sigma; > 0</b>).")
         self.variable_3_info.setReadOnly(True)
         self.right_group_box_layout.addWidget(self.variable_3_info,2,1)
 
-
-        self.variable_4_info_label = QLabel("4")
+        
+        self.variable_4_info_label = QLabel("x")
         self.right_group_box_layout.addWidget(self.variable_4_info_label,3,0)
 
-        self.variable_4_info = QTextEdit()
+        self.variable_4_info = QTextEdit("<b>x (Target Value):</b><br>"
+                                        "The specific value on the continuous scale that you are evaluating.")
         self.variable_4_info.setReadOnly(True)
         self.right_group_box_layout.addWidget(self.variable_4_info,3,1)
 
@@ -118,67 +125,111 @@ class OperationWidget(QWidget):
 
         self.variable_1_input.textChanged.connect(self.reset_and_update_display)
         self.variable_2_input.textChanged.connect(self.reset_and_update_display)
-        self.variable_3_input.textChanged.connect(self.reset_and_update_display)
-
+        self.variable_3_input.currentTextChanged.connect(self.reset_and_update_display)
 
     def reset_and_update_display(self):
         self.current_result = "<span style='color: gray;'><i>Waiting...</i></span>"
         self.update_formula_display()
 
-
     def update_formula_display(self):
-            variable_1 = self.variable_1_input.text() or "α"
-            variable_2 = self.variable_2_input.text() or "x<sub>m</sub>"
-            variable_3 = self.variable_3_input.text() or "x"
+        self.variable_3 = self.variable_3_input.currentText()
+        self.variable_4_display =  self.variable_2_input.text().strip() or "x"
+        if self.variable_3 == "Population":
+            self.variable_2_display = "<i>&sigma;</i>"
+            self.variable_1_display = "<i>&mu;</i>"
+        else:
+            self.variable_2_display = "s"
+            self.variable_1_display = "<i>x&#772;</i>"
+        
+        raw_text = self.variable_1_input.toPlainText().strip()
+        if not raw_text:
+            pass
+        else : 
+            try:
+                data = [float(x.strip()) for x in raw_text.split(",") if x.strip()]
+                data = [math.log(d) for d in data if d > 0]
+                if not data:
+                    raise ValueError
+                self.variable_1 = sum(data) / len(data)
+                self.variable_1_display = f"{self.variable_1:.2f}"
+                if self.variable_3 == "Population":
+                        self.variable_2 = statistics.pstdev(data)
+                else:
+                    self.variable_2 = statistics.stdev(data)
+                    self.variable_2_display = f"{self.variable_2:.2f}"     
+            except ValueError:
+                self.current_result = "<span style='color: #EF4444; font-size: 20px;'>Invalid Input! All Values Must be > 0</span>"
+                self.variable_1 = "<i>&mu;</i>"
+                return
 
-            html_formul = f"""
-            <table align="center" cellpadding="0" cellspacing="0">
+
+        html_formul = f"""
+            <table align="center" cellpadding="0" cellspacing="0" style="font-size: 45px; font-family: 'Times New Roman', serif;">
                 <tr>
-                    <td valign="middle" style="padding-right: 10px;">
-                        <i>f({variable_3})</i> = 
+                    <td valign="middle" style="padding-right: 22px;">
+                        <i>f({self.variable_4_display})</i> = 
                     </td>
                     
                     <td valign="middle">
-                        <table cellpadding="0" cellspacing="0">
+                        <table cellpadding="0" cellspacing="0" style="font-size: 36px;">
                             <tr>
-                                <td align="center" style="border-bottom: 2px solid currentColor; padding: 0px 8px;">
-                                    {variable_1} &middot; {variable_2}<sup>{variable_1}</sup>
+                                <td align="center" style="border-bottom: 3px solid currentColor; padding: 0px 12px 6px 12px;">
+                                    1
                                 </td>
                             </tr>
                             <tr>
-                                <td align="center" style="padding: 4px 8px 0px 8px;">
-                                    {variable_3}<sup>{variable_1} + 1</sup>
+                                <td align="center" style="padding: 6px 12px 0px 12px;">
+                                    {self.variable_4_display}&times;{self.variable_2_display}&radic;<span style="text-decoration: overline;">2&pi;</span>
                                 </td>
                             </tr>
                         </table>
                     </td>
 
-                    <td valign="middle" style="padding-left: 10px;">
+                    <td valign="middle" style="padding-left: 15px; font-size: 51px;">
+                        <i>e</i>
+                    </td>
+
+                    <td valign="top" style="padding-top: 8px;"> <table cellpadding="0" cellspacing="0" style="font-size: 27px;">
+                            <tr>
+                                <td rowspan="2" valign="middle" style="padding-right: 8px; font-size: 33px;">
+                                    &minus;
+                                </td>
+                                <td align="center" style="border-bottom: 2px solid currentColor; padding: 0px 6px 3px 6px;">
+                                    (ln{self.variable_4_display} &minus; {self.variable_1_display})<sup>2</sup>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center" style="padding: 3px 6px 0px 6px;">
+                                    2&times;{self.variable_2_display}<sup>2</sup>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+
+                    <td valign="middle" style="padding-left: 30px;">
                         = {self.current_result}
                     </td>
                 </tr>
             </table>
-            """
-            self.dynamic_formula.setText(html_formul)
+        """
+        self.dynamic_formula.setText(html_formul)
 
     def calculate_button_function(self):
         try:
-            variable_1 = float(self.variable_1_input.text())
-            variable_2 = float(self.variable_2_input.text())
-            variable_3 = float(self.variable_3_input.text())
-            variable_4 = float(self.variable_4_input.text())
-
-            if variable_3 < variable_2:
-                 self.current_result = "<span style='color: #EF4444; font-size: 20px;'>x >= Xm</span>"
-                
+            x = float(self.variable_4_display)
+            if x <= 0:result = 0
             else:
+                result = (1 / (x * self.variable_2 * math.sqrt(2 * math.pi))) * (math.e ** (-((math.log(x) - self.variable_1)**2) / (2 * (self.variable_2 ** 2))))
 
-                result = (variable_1 * (variable_2 ** variable_1)) / (variable_3 ** (variable_1 + 1))
-
-                self.current_result = f"<span style='color: #10B981; font-weight: bold;'>{result:.4f}</span>"
+            self.current_result = f"<span style='color: #10B981; font-weight: bold;'>{result:.4f}</span>"
 
         except ValueError:
-            self.current_result = "<span style='color: #EF4444; font-size: 20px;'>Invalid Number!</span>"
+            self.current_result = "<span style='color: #EF4444; font-size: 20px;'>Invalid Input!</span>"
+        except ZeroDivisionError:
+            self.current_result = "<span style='color: #EF4444; font-size: 20px;'>Div by Zero!</span>"
+        except Exception:
+             self.current_result = "<span style='color: #EF4444; font-size: 20px;'>Error!</span>"
+
         self.update_formula_display()
 
 
